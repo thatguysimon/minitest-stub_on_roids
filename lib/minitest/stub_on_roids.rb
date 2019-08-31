e08d0adf75cfb3f3d91c4997cb4862173503b1b9
Simon Nizov <simon.nizov@gmail.com>
2019-08-31 17:42:41 +0300

    Better variable names

--- lib/minitest/stub_on_roids.rb
+++ lib/minitest/stub_on_roids.rb
@@ -5,35 +5,35 @@ class MethodAlreadyStubbedError < StandardError; end
 
 module Minitest
   module StubOnRoids
-    def stub_with_args(name, val_or_callable, expected_args, &block)
+    def stub_with_args(name, retval, expected_args = [], &block)
       raise MethodAlreadyStubbedError, "Method :#{name} already stubbed" if self.respond_to? "__minitest_stub__#{name}"
 
-      asserted_callable = lambda do |*actual_args|
+      retval_with_args_assertion = lambda do |*actual_args|
         expected_args.zip(actual_args).each do |expected_arg, actual_arg|
           next if expected_arg == actual_arg
           raise StubbedMethodArgsError, "stubbed method :#{name} called on #{self} with unexpected arguments #{actual_args}"
         end
         
-        val_or_callable
+        retval
       end
 
-      stub(name, asserted_callable) do
+      stub(name, retval_with_args_assertion) do
         yield
       end
     end
 
-    def stub_and_expect(name, val_or_callable = nil, expected_args = [], times: 1, expectations: [])
+    def stub_and_expect(name, retval = nil, expected_args = [], times: 1, expectations: [], &block)
       raise MethodAlreadyStubbedError, "Method :#{name} already stubbed" if self.respond_to? "__minitest_stub__#{name}"
       
       mock = Minitest::Mock.new
 
 
-      if (!val_or_callable.nil? || expected_args.size.positive?) && expectations.size.positive?
-        raise ArgumentError, "`val_or_callable` and `expected_args` arguments cannot be passed along with `expectations`"
+      if (!retval.nil? || expected_args.size.positive?) && expectations.size.positive?
+        raise ArgumentError, "`retval` and `expected_args` arguments cannot be passed along with `expectations`"
       end
 
       if expectations.size.zero?
-        times.times { mock.expect :call, val_or_callable, expected_args }
+        times.times { mock.expect :call, retval, expected_args }
       else
         expectations.each do |expectation|
           [:expected_args, :return_value].each { |k| raise ArgumentError, "Missing key #{k} in expectation definition" if expectation[k].nil? }
